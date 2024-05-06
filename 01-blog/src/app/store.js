9f6e14ddf81914dffb62ee920328763489d69d85
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-05 23:33:09 -0500

    feat: Add the authorsSlice to the store

--- 01-blog/src/app/store.js
+++ 01-blog/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsSlice from "../features/posts/postsSlice";
+import authorsSlice from "../features/authors/authorsSlice";
 
 const store = configureStore({
   reducer: {
     posts: postsSlice,
+    authors: authorsSlice,
   },
 });
 
