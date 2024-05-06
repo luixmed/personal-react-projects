cb23b054f6e564e94de9117efc31a3ba07b20b55
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-05 23:31:05 -0500

    feat: Add Post form component

---
+++ 01-blog/src/features/posts/AddPostForm.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { AddPostFormStyled } from "./AddPostFormStyles";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllAuthors } from "../authors/authorsSlice";
+import { addPost } from "./postsSlice";
+
+function AddPostForm() {
+  const [title, setTitle] = useState("");
+  const [authorId, setAuthorId] = useState("");
+  const [content, setContent] = useState("");
+
+  const authors = useSelector(getAllAuthors);
+
+  const dispatch = useDispatch();
+
+  function handleSavePost() {
+    if (!title || !content) return;
+
+    dispatch(addPost(title, content));
+    setTitle("");
+    setContent("");
+  }
+
+  return (
+    <AddPostFormStyled>
+      <h2>Add a new post</h2>
+      <form>
+        {/* TITLE */}
+        <div>
+          <label htmlFor="postTitle">Post Title</label>
+          <input
+            type="text"
+            id="postTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        {/* AUTHOR */}
+        <div>
+          <label htmlFor="postAuthor">Author</label>
+          <select
+            id="postAuthor"
+            value={authorId}
+            onChange={(e) => setAuthorId(e.target.value)}
+          >
+            <option value=""></option>
+            {authors.map((author) => (
+              <option key={author.id} value={author.id}>
+                {author.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* CONTENT */}
+        <div>
+          <label htmlFor="postContent">Content</label>
+          <textarea
+            id="postContent"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            rows={5}
+          />
+        </div>
+
+        {/* BUTTON */}
+        <button
+          type="button"
+          onClick={handleSavePost}
+          disabled={!title || !content || !authorId}
+        >
+          Save post
+        </button>
+      </form>
+    </AddPostFormStyled>
+  );
+}
+
+export default AddPostForm;
---
+++ 01-blog/src/features/posts/AddPostFormStyles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const AddPostFormStyled = styled.section`
+  /* Box */
+  padding: 1.6rem 2.4rem;
+
+  /* Style */
+  background-color: #444;
+  h2 {
+    font-size: 3rem;
+    text-transform: uppercase;
+
+    margin-bottom: 2.4rem;
+  }
+
+  form {
+    /* Layout */
+    display: flex;
+    flex-direction: column;
+    gap: 2.4rem;
+
+    div {
+      display: grid;
+      grid-template-columns: 8rem 1fr;
+    }
+  }
+
+  input,
+  select {
+    color: inherit
+    font-family: inherit;
+    font-size: 1.6rem;
+
+    padding: 0.8rem;
+  }
+
+  button {
+    align-self: center;
+
+    color: #fff4e6;
+    font-size: 1.4rem;
+    font-weight: 600;
+    text-transform: uppercase;
+
+    padding: 1rem 2rem;
+    border: none;
+    border-radius: 10rem;
+
+    background-color: #e67700;
+  }
+`;
